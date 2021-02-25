f3f193be1632588727674d1fb836ec28f2a2bedb
skysurveil <skylerrouton@protonmail.com>
2021-02-25 14:07:55 -0800

    Added js that dynamically displays the text that the user submitted

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 function clearText(element) {
   element.value = '';
 }
+
+function getText() {
+  var textInput = document.getElementById("textArea").value;
+  console.log(textInput);
+
+  resultTitle = document.createElement('p');
+  resultTitle.innerHTML = "Your input:";
+
+  resultP = document.createElement('p');
+  resultP.innerHTML = textInput;
+
+  document.body.appendChild(resultTitle);
+  document.body.appendChild(resultP);
+}
