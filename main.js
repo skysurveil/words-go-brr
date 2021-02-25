7e2540d9a1077fd70f5e8af0d4d4b07272db23b7
Dustie <dustinh6069@gmail.com>
2021-02-25 15:27:40 -0800

    started a printing words function

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var textblock;
+
 /* clearText clears the textArea default text when focused */
 function clearText(element) {
   element.value = '';
@@ -7,11 +9,24 @@ assigns title text and user input to titleElement and resultElement
 paragraph elements to display the user's input.*/
 function getText() {
   var textInput = document.getElementById("textArea").value;
-  console.log(textInput);
-
+  //console.log(textInput);
+  textblock = textInput;
   titleElement = document.getElementById("resultTitle");
-  titleElement.innerHTML = "Your input:";
+  //titleElement.innerHTML = "Your input:";
 
   resultElement = document.getElementById("resultP");
-  resultElement.innerHTML = textInput;
+  //resultElement.innerHTML = textInput;
+  readText();
+}
+
+
+function readText(){
+  //console.log(textblock.split(" ").length);
+  let words = textblock.split(" ");
+  print = document.getElementById("resultP");
+  for(var i=0; i<words.length; i++){
+    console.log(words[i]);
+    print.innerHTML = words[i];
+  }
+
 }
