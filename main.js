34399c5988636da391dec4c5c2800ed7716617c5
skysurveil <skylerrouton@protonmail.com>
2021-02-25 15:12:58 -0800

    Fixed results so additional entries are no longer created upon successive submissions. Also fixed formatting of output text

--- main.js
+++ main.js
@@ -3,19 +3,15 @@ function clearText(element) {
   element.value = '';
 }
 /* getText grabs the user text from textArea, saves to var "textInput",
-creates two p elements, one for title, one for user input. Assigns title
-and user input to the p elements, appends p elements to the HTML body
-to display the user's inputted text.*/
+assigns title text and user input to titleElement and resultElement
+paragraph elements to display the user's input.*/
 function getText() {
   var textInput = document.getElementById("textArea").value;
   console.log(textInput);
 
-  resultTitle = document.createElement('p');
-  resultTitle.innerHTML = "Your input:";
+  titleElement = document.getElementById("resultTitle");
+  titleElement.innerHTML = "Your input:";
 
-  resultP = document.createElement('p');
-  resultP.innerHTML = textInput;
-
-  document.body.appendChild(resultTitle);
-  document.body.appendChild(resultP);
+  resultElement = document.getElementById("resultP");
+  resultElement.innerHTML = textInput;
 }
