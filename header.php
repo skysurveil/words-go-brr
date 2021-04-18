680223323c07642fc7ea9df04f4463447b6a4d38
pgzman112 <pgzman112@gmail.com>
2021-04-18 15:12:16 -0700

    doing a wild shift

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Words Go Brr</title>
+    <link rel="stylesheet" href="styles.css" id="theme">
+  </head>
+
+  <!--This is some stuff for getting into php-->
+
+  <nav>
+    <div class="wrapper">
+      <a href="index.php"></a>
+      <ul>
+        <li><a href= "index.php">Home</a></li>
+        <li><a href='signup.php'>Sign Up</a></li>
+        <li><a href='login.php'>Log In</a></li>
+      </ul>
+    </div>
+  </nav>
---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+  include_once 'header.php';
+?>
+
+  <script src="main.js" type="text/javascript"></script>
+  <body>
+    <h1>Words Go Brr</h1>
+    <form method="GET">
+      <textarea id="textArea" onfocus="clearText(this);" name="Input" rows="8" cols="40">Paste words to go brr...</textarea>
+      <br/>
+      <input onclick="getText(); return false;" id="submitButton" type="button" name="submit" value="Read text!"/>
+    </form>
+    <button onclick="replay()" id="replay">replay</button>
+    <br/>
+    <button onclick="getTextTTS()" id="swap">Read the text for me!</button>
+    <div class="slidecontainer">
+        <p>Display WPM: <span id="wordspermin"></span></p>
+        <input type="range" min="200" max="500" step="10" class="slider" id="myRange" value="350" >
+
+      </div>
+      <script src="main.js"></script>
+      <script src='https://unpkg.com/tesseract.js@v2.1.0/dist/tesseract.min.js'></script>
+      <p id="resultTitle"></p>
+      <p id="resultP"></p>
+      <p><input type="file"  accept="image/*" name="image" id="file"  onchange="uploadImage(event)" ></p>
+      <p><img id="output" width="500" /></p>
+      <button onclick="swap()" id="swap">Toggle dark mode</button>
+      <br/>
+      <br/>
+
+
+      <form id="ttsSpeeds">
+        <legend>Text to Speech reading speed</legend>
+        <label><input type="radio" name="ttsOption" value="0.8">Slow</label>
+        <label><input type="radio" name="ttsOption" value="1" checked="checked">Normal</label>
+        <label><input type="radio" name="ttsOption" value="1.5">Medium</label>
+        <label><input type="radio" name="ttsOption" value="2">Fast</label>
+      </form>
+
+      <br/>
+      <br/>
+
+      <legend>Alternate Themes</legend>
+      <select name="bgTheme" id="backgroundTheme">
+        <option value="none">none</option>
+        <option value="blue">Blue</option>
+        <option value="red">Red</option>
+        <option value="green">Green</option>
+      </select>
+      <input onclick="changeTheme()" type="submit" value="Apply">
+
+  </body>
+</div>
+</html>
